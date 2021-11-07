c8c32aad618f48c2b0dd419a6a6c6c7137dcea6e
Philipp Fröbel <philipp@snowhills.de>
2021-11-07 10:09:55 +0100

    test isValidDateString helper

---
+++ test/Helpers.test.ts
@@ -0,0 +1,11 @@
+import { isValidDateString } from "../src/utils/Helpers";
+
+describe("test isValidDateString helper", () => {
+    it("should be a valid date string", () => {
+        expect(isValidDateString("1970-01-10 00:00:00")).toBeTruthy();
+    });
+
+    it("should not be a valid date string", () => {
+        expect(isValidDateString("1970:01:10 00:00:00")).toBeFalsy();
+    });
+});
