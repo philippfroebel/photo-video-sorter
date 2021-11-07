23cf26f1bd69535574dffbd1ec13f9d48a6b9425
Philipp Fröbel <philipp@snowhills.de>
2021-11-07 19:16:01 +0100

    exif date fixer test

--- test/utils/Helpers.test.ts
+++ test/utils/Helpers.test.ts
@@ -1,4 +1,4 @@
-import { isValidDateString } from "../../src/utils/Helpers";
+import { fixExifDateString, isValidDateString } from "../../src/utils/Helpers";
 
 describe("test isValidDateString helper", () => {
     it("should be a valid date string", () => {
@@ -7,5 +7,12 @@ describe("test isValidDateString helper", () => {
 
     it("should not be a valid date string", () => {
         expect(isValidDateString("1970:01:10 00:00:00")).toBeFalsy();
+    });
+});
+
+describe("test fixExifDateString helper", () => {
+    it("should be return valid date string", () => {
+        expect(fixExifDateString("1970:01:10 00:00:00")).toBe("1970-01-10");
+        expect(fixExifDateString("1970-01-10 00:00:00")).toBe("1970-01-10");
     });
 });
